c4645548588ed014513667259be96e613fb5947e
Chang-Hong Chen <longhongc@gmail.com>
2022-12-15 04:33:28 -0500

    Add Doxygen comment

--- robot_manager/include/robot_manager/nav_action_client.hpp
+++ robot_manager/include/robot_manager/nav_action_client.hpp
@@ -22,37 +22,129 @@ using std::chrono_literals::operator""ms;
 using NavToPoseAction = nav2_msgs::action::NavigateToPose;
 using GoalHandleNavToPose = rclcpp_action::ClientGoalHandle<NavToPoseAction>;
 
+/**
+ * @Brief  The internal state of the NavActionClient class
+ */
 enum class ClientState;
 
+/**
+ * @Brief  A client interface for sending action to Nav2
+ */
 class NavActionClient
 {
 public:
+  /**
+   * @Brief  Constructor
+   *
+   * @Param node  This class is a subclass in robot manager.
+   *              It requires a shared pointer of the robot manager
+   *              class to use some of the functions.
+   */
   explicit NavActionClient(std::shared_ptr<rclcpp::Node> node);
+
+  /**
+   * @Brief  Destructor
+   */
   ~NavActionClient();
+
+  /**
+   * @Brief  Initialization of the NavACtionClient class
+   *
+   * @Returns
+   */
   bool initialize();
+
+  /**
+   * @Brief  Reset the action client state
+   *         Prepare for a new round of action request
+   */
   void reset();
+
+  /**
+   * @Brief  Send action request
+   *
+   * @Param waypoint The target pose to move to
+   *
+   * @Returns  True if send goal success
+   */
   bool sendGoal(geometry_msgs::msg::PoseStamped & waypoint);
+
+  /**
+   * @Brief  Check if NavActionClient is executing a waypoint request
+   *
+   * @Returns  True if NavActionClient is executing the previous waypoint
+   *           command
+   */
   bool onTask();
+
+
+  /**
+   * @Brief  Check if NavActionClient has finished the previous command
+   *
+   * @Returns  True if NavActionClient has finished the prevous command
+   */
   bool getResult();
 
 private:
+  /**
+   * @Brief Callback function for goal response from action server
+   *
+   * @Param future Goal handler for action client
+   */
   void goalResponseCallback(std::shared_future<GoalHandleNavToPose::SharedPtr> future);
+
+  /**
+   * @Brief Callback function for feedback from action server
+   *
+   * @Param GoalHandleNavToPose::SharedPtr
+   * @Param feedback Contains information of
+   *        robot pose, execution time, and numbers of recoveries
+   */
   void feedbackCallback(
     GoalHandleNavToPose::SharedPtr,
     const std::shared_ptr<const NavToPoseAction::Feedback> feedback);
+
+  /**
+   * @Brief  Callback function for results from action server
+   *
+   * @Param result Whether the goal has been successfully executed
+   */
   void resultCallback(const GoalHandleNavToPose::WrappedResult & result);
 
+  /**
+   * @Brief  Shared pointer of the node for sub class argument
+   */
   std::shared_ptr<rclcpp::Node> node_;
 
+  /**
+   * @Brief  The current execution state
+   */
   ClientState client_state_;
 
+  /**
+   * @Brief  navigate_to_pose action client
+   */
   rclcpp_action::Client<NavToPoseAction>::SharedPtr client_ptr_;
 
+  /**
+   * @Brief  Current robot pose
+   */
   geometry_msgs::msg::PoseStamped current_pose_;
+
+  /**
+   * @Brief  Current waypoint pose
+   */
   geometry_msgs::msg::PoseStamped goal_pose_;
+
+  /**
+   * @Brief  Navigation time for current waypoint
+   */
   rclcpp::Duration navigation_time_{0s};
 
   // Todo: pass in navigation timeout
+  /**
+   * @Brief  Timeout for robot failure on waypoint
+   */
   rclcpp::Duration NAVIGATION_TIMEOUT{300s};
 };
 #endif  // ROBOT_MANAGER__NAV_ACTION_CLIENT_HPP_
--- robot_manager/include/robot_manager/robot_manager.hpp
+++ robot_manager/include/robot_manager/robot_manager.hpp
@@ -22,52 +22,152 @@
 #include "robot_manager/nav_action_client.hpp"
 #include "robot_manager_msgs/srv/set_routine.hpp"
 
+/**
+ * @Brief  The internal state of RobotManager class
+ */
 enum class RobotManagerState;
+
+/**
+ * @Brief  The state of a waypoint
+ *
+ */
 enum class WaypointState;
 
 using SetRoutine = robot_manager_msgs::srv::SetRoutine;
 
+/**
+ * @Brief  A interface to control and interact with the robot
+ */
 class RobotManager : public rclcpp::Node
 {
 public:
+  /**
+   * @Brief  Constructor
+   */
   RobotManager();
 
 private:
+  /**
+   * @Brief  Initialize the RobotManager class
+   */
   void initialize();
+
+  /**
+   * @Brief  Main finite state machine control loop
+   */
   void controlCycleCallback();
+
+  /**
+   * @Brief Execute a routine
+   */
   void runRoutine();
+
+  /**
+   * @Brief Execute a waypoint
+   *
+   * @Returns True if a waypoint command is sent successfully
+   */
   bool runWaypoint();
+
+  /**
+   * @Brief Reset the RobotManager class for a new round of routine
+   */
   void reset();
 
+  /**
+   * @Brief Callback function for timer to update marker on RVIZ
+   */
   void waypointsMarkerCallback();
+
+  /**
+   * @Brief  Callback for set_routine service
+   *
+   * @Param request The routine string vector
+   * @Param response Whether service is sent successfully
+   */
   void setRoutineCallback(
     const std::shared_ptr<SetRoutine::Request> request,
     std::shared_ptr<SetRoutine::Response> response);
 
+  /**
+   * @Brief  The current execution state of RobotManager
+   */
   RobotManagerState robot_manager_state_;
 
+  /**
+   * @Brief  The main control loop timer
+   */
   rclcpp::TimerBase::SharedPtr control_cycle_timer_;
+
+  /**
+   * @Brief  An action client subclass to communicate with Nav2
+   */
   std::shared_ptr<NavActionClient> nav_to_pose_client_;
 
+  /**
+   * @Brief  Marker publisher to publish waypoints to RVIZ
+   */
   rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr waypoints_marker_pub_;
+
+  /**
+   * @Brief  Timer for updating marker
+   */
   rclcpp::TimerBase::SharedPtr waypoints_marker_timer_;
 
+  /**
+   * @Brief  Service server of set_routine
+   */
   rclcpp::Service<SetRoutine>::SharedPtr set_routine_service_;
 
-  // Todo: readin parameters
+  /**
+   * @Brief  The name of all waypoints
+   */
   std::vector<std::string> waypoints_name_;
+
+  /**
+   * @Brief  A dictionary that map waypoint name to its pose
+   */
   std::map<std::string, geometry_msgs::msg::PoseStamped> waypoint_name_to_pose_;
+
+  /**
+   * @Brief  A dictionary that map waypoint name to its id
+   */
   std::map<std::string, int> waypoint_name_to_index_;
+
+  /**
+   * @Brief  The state of every waypoints, whether it is in task queue or finished
+   */
   std::map<std::string, WaypointState> waypoints_state_;
 
+  /**
+   * @Brief  The routine sequence in name
+   */
   std::vector<std::string> routine_by_name_;
+
+  /**
+   * @Brief  The routine sequence in pose
+   */
   std::vector<geometry_msgs::msg::PoseStamped> routine_by_pose_;
 
+  /**
+   * @Brief  The current waypoints queue that contains waypoints
+   *         ready to be executed
+   */
   std::queue<std::string> current_queue_;
+
+  /**
+   * @Brief  The current waypoint that is executing
+   */
   std::string current_waypoint_;
 
+  /**
+   * @Brief  The origin of the robot with respect to map
+   */
   geometry_msgs::msg::Pose origin_;
 
+  /**
+   * @Brief  Timeout for robot failure on waypoint
+   */
   rclcpp::Duration NAVIGATION_TIMEOUT{20s};
 };
 
